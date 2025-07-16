932db0fbf63d2bee4dd4d37131df81f54bb5d6d8
tryingtodosth <p.putylo96@gmail.com>
2025-07-16 12:25:39 +0200

    tasks are linked to proper user models (not obsolete ones

--- task/models.py
+++ task/models.py
@@ -1,16 +1,21 @@
 from django.db import models
+from django.utils import timezone
+from django.conf import settings
 
-# Create your models here.
 class Task(models.Model):
     name = models.CharField(max_length=50)
     desc = models.TextField("description")
     priority = models.IntegerField(null=True)
-    created_by = models.ForeignKey("user.User", on_delete=models.CASCADE)
+    created_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL, 
+        null=True, 
+        blank=True, 
+        on_delete=models.SET_NULL,
+        related_name='created_tasks'
+    )
     created_at = models.DateTimeField(auto_now_add=True)
-    contributions = models.JSONField(
-        null=True
-    ) 
-
+    contributions = models.JSONField(null=True)
+    
     to_project = models.ForeignKey("project.Project", blank=True, null=True, on_delete=models.CASCADE)
     to_task = models.ForeignKey("task.Task", blank=True, null=True, on_delete=models.CASCADE)
 
