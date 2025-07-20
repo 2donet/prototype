174f54011e42a07dfde582f6e527c10f29b63edd
tryingtodosth <p.putylo96@gmail.com>
2025-07-20 09:50:17 +0200

    small UI tweaks

--- project/urls.py
+++ project/urls.py
@@ -7,16 +7,18 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("<int:project_id>/", views.project, name="project"),
     path('create/', views.create_project, name='create_project'),
-    path('skill/<str:skill_name>/', views.skill_projects, name='skill_detail'),
     path('api/', include('comment.api_urls')),
+    
+    path('skill/<str:skill_name>/', views.skill_projects, name='skill_detail'), #list of projects with a selected skill
 
-    # Member management URLs
+    path('<int:project_id>/join/', views.join_project, name='join_project'),
+    path('<int:project_id>/leave/', views.leave_project, name='leave_project'),
+    
     path('<int:project_id>/members/', views.project_members, name='project_members'),
     path('<int:project_id>/members/<int:user_id>/', views.member_detail, name='member_detail'),
     path('<int:project_id>/members/add/', views.add_member, name='add_member'),
     path('<int:project_id>/members/<int:user_id>/remove/', views.remove_member, name='remove_member'),
-    path('<int:project_id>/join/', views.join_project, name='join_project'),
-    path('<int:project_id>/leave/', views.leave_project, name='leave_project'),
+
     path('<int:project_id>/localizations/add/', views.add_localization, name='add_localization'),
     path('<int:project_id>/localizations/<int:localization_id>/edit/', views.edit_localization, name='edit_localization'),
     path('<int:project_id>/localizations/<int:localization_id>/delete/', views.delete_localization, name='delete_localization'),
